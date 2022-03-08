2532efc7c65cff176e0c712406f6c1fb4a0b793f
samanmohammadraouf <www.saman.raouf@gmail.com>
2022-03-08 20:05:52 +0330

    controllers for form input validations

---
+++ app/src/main/java/com/example/tolearn/LoginSignUpInputController.java
@@ -0,0 +1,51 @@
+package com.example.tolearn;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+
+public class LoginSignUpInputController {
+    public static boolean IsUsernameCorrect(String username)
+    {
+        if(username.length()<8)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static boolean checkStringPassword(String str) {
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        for(int i=0;i < str.length();i++) {
+            ch = str.charAt(i);
+            if( Character.isDigit(ch)) {
+                numberFlag = true;
+            }
+            else if (Character.isUpperCase(ch)) {
+                capitalFlag = true;
+            } else if (Character.isLowerCase(ch)) {
+                lowerCaseFlag = true;
+            }
+            if(numberFlag && capitalFlag && lowerCaseFlag)
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+}

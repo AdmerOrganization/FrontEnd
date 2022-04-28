d96f1c5b92816b701c831271d9f6755545d27fec
samanmohammadraouf <saman.raouf@gmail.com>
2022-04-28 20:39:00 +0430

    homework creation form design

--- app/src/main/java/com/example/tolearn/ClassProfileActivity.java
+++ app/src/main/java/com/example/tolearn/ClassProfileActivity.java
@@ -1,22 +1,21 @@
 package com.example.tolearn;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tolearn.databinding.ActivityClassProfileBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
 public class ClassProfileActivity extends AppCompatActivity {

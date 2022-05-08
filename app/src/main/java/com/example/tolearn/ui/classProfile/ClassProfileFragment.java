d2fe34ecf142f8f84cbee1eaa36590aa03937bb5
samanmohammadraouf <saman.raouf@gmail.com>
2022-05-08 11:35:23 +0430

    class profile info fragment

---
+++ app/src/main/java/com/example/tolearn/ui/classProfile/ClassProfileFragment.java
@@ -0,0 +1,15 @@
+package com.example.tolearn.ui.classProfile;
+
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ClassProfileFragment extends Fragment {
+    TextView classroom_title;
+    TextView classroom_teacher;
+    TextView classroom_desc;
+    TextView classroom_limit;
+    ListView classroom_members;
+    
+}

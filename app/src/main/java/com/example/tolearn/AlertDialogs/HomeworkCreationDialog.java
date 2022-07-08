f2046ef5f36afbc334beb6492c3c9c2d0e55ecc2
samanmohammadraouf <saman.raouf@gmail.com>
2022-07-08 21:46:39 +0430

    homework create bug fixed

--- app/src/main/java/com/example/tolearn/AlertDialogs/HomeworkCreationDialog.java
+++ app/src/main/java/com/example/tolearn/AlertDialogs/HomeworkCreationDialog.java
@@ -1,6 +1,7 @@
 package com.example.tolearn.AlertDialogs;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -20,14 +22,35 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 
 import com.example.tolearn.Controllers.homework_creation_validations;
-import com.example.tolearn.Entity.Homework;
 import com.example.tolearn.R;
 import com.example.tolearn.webService.HomeworkAPI;
-import com.google.gson.JsonObject;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
@@ -38,25 +61,19 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import com.example.tolearn.webService.VolleyMultipartRequest;
+
 public class HomeworkCreationDialog extends Activity {
+    private static String pdfFileName;
     public EditText titleET;
     public EditText descET;
     public DatePicker datePicker;
@@ -65,10 +82,20 @@ public class HomeworkCreationDialog extends Activity {
     HomeworkAPI homeworkAPI;
     private static final int BUFFER_SIZE = 1024 * 2;
     private static final String IMAGE_DIRECTORY = "/Download";
+    private static final String upload_URL = "http://185.235.42.101:8000/homeworks/create/";
     Bundle extras;
     int class_id;
     File file;
     String path;
+    Uri fileuri;
+
+    //new method ...
+    String displayName = null;
+    Uri uri;
+
+    private RequestQueue rQueue;
+    private ArrayList<HashMap<String, String>> arraylist;
+
     public homework_creation_validations Controller;
     private static final int PICK_PDF_FOR_FILE = 0;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -148,25 +175,57 @@ public class HomeworkCreationDialog extends Activity {
 //            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
 //        }
 //    }
+    @SuppressLint("Range")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (resultCode == RESULT_OK) {
+//            // Get the Uri of the selected file
+//            fileuri = data.getData();
+//            String uriString = fileuri.toString();
+//
+//            File myFile = new File(uriString);
+//            path = getFilePathFromURI(this,fileuri);
+//            Log.d("iooooooooooooooooooooo",path);
+//            //uploadPDF(path);
+//        }
+//
+//        super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode == RESULT_OK) {
             // Get the Uri of the selected file
-            Uri uri = data.getData();
+            uri = data.getData();
             String uriString = uri.toString();
-
             File myFile = new File(uriString);
-            path = getFilePathFromURI(this,uri);
-            Log.d("iooooooooooooooooooooo",path);
-            //uploadPDF(path);
+            String path = myFile.getAbsolutePath();
+
+
+            if (uriString.startsWith("content://")) {
+                Cursor cursor = null;
+                try {
+                    cursor = this.getContentResolver().query(uri, null, null, null, null);
+                    if (cursor != null && cursor.moveToFirst()) {
+                        displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                        Log.d("nameeeee>>>>  ",displayName);
+
+                        //uploadPDF(displayName,uri);
+                    }
+                } finally {
+                    cursor.close();
+                }
+            } else if (uriString.startsWith("file://")) {
+                displayName = myFile.getName();
+                Log.d("nameeeee>>>>  ",displayName);
+            }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+
 
     }
     public static String getFilePathFromURI(Context context, Uri contentUri) {
         //copy file and send new file path
         String fileName = getFileName(contentUri);
+        pdfFileName = fileName;
         File wallpaperDirectory = new File(
                 Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
 
@@ -340,8 +399,13 @@ public class HomeworkCreationDialog extends Activity {
             @Override
             public void onClick(View view) {
 
-                browseDocuments();
+                //browseDocuments();
+                requestMultiplePermissions();
 
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/pdf");
+                startActivityForResult(intent,1);
             }
         });
 
@@ -359,48 +423,10 @@ public class HomeworkCreationDialog extends Activity {
                 } else if (!Controller.IsDateValid(year, month, day)) {
                     Toast.makeText(HomeworkCreationDialog.this, "You can not select a date in the past.", Toast.LENGTH_SHORT).show();
                 } else {
-                    if(!path.equals(""))
+                    if(!uri.equals(""))
                     {
-                        File file = new File(path);
-                        RequestBody requestFile =
-                                RequestBody.create(MediaType.parse("*/*"), file);
-
-// MultipartBody.Part is used to send also the actual file name
-                        MultipartBody.Part body =
-                                MultipartBody.Part.createFormData("file", file.getName(), requestFile);
-                        RequestBody titleR =
-                                RequestBody.create(MediaType.parse("multipart/form-data"), titleET.getText().toString());
-                        RequestBody descR =
-                                RequestBody.create(MediaType.parse("multipart/form-data"), descET.getText().toString());
-                        RequestBody dateR =
-                                RequestBody.create(MediaType.parse("multipart/form-data"), year+"-"+month+"-"+day);
-                        SharedPreferences shP = getSharedPreferences("userInformation", MODE_PRIVATE);
-                        String token = shP.getString("token", "");
-                        Call<JsonObject> homeworkCall = homeworkAPI.Create("token " + token, titleR, descR, dateR,class_id, body);
-                        Log.i("5", "5");
-                        homeworkCall.enqueue(new Callback<JsonObject>() {
-                            @Override
-                            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                                if (!response.isSuccessful()) {
-                                    Toast.makeText(HomeworkCreationDialog.this, response.message(), Toast.LENGTH_SHORT).show();
-                                    Log.i("MOSHKEL", response.message());
-                                } else {
-                                    String code = Integer.toString(response.code());
-                                    JsonObject homework = response.body();
-                                    Log.i("PHOTO", "SUCCED");
-                                    //                           Log.i("IMAGE URL",user.getAvatar().toString());
-                                    Toast.makeText(HomeworkCreationDialog.this, "Homework created!", Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<JsonObject> call, Throwable t) {
-                                Log.i("moshkel","injas");
-                                Log.i("Moshkel",t.getMessage());
-                                Toast.makeText(HomeworkCreationDialog.this, "error is :" + t.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
+                        uploadPDF(displayName , uri);
+                        finish();
                     }
                     else{
                         Toast.makeText(HomeworkCreationDialog.this, "select a pdf file", Toast.LENGTH_SHORT).show();
@@ -409,6 +435,141 @@ public class HomeworkCreationDialog extends Activity {
                 }
             }
         });
+
+    }
+
+    private void uploadPDF(final String pdfname, Uri pdffile){
+
+        InputStream iStream = null;
+        try {
+
+            iStream = getContentResolver().openInputStream(pdffile);
+            final byte[] inputData = getBytes(iStream);
+
+            VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, upload_URL,HomeworkCreationDialog.this,
+                    new Response.Listener<NetworkResponse>() {
+                        @Override
+                        public void onResponse(NetworkResponse response) {
+                            Log.d("ressssssoo",new String(response.data));
+                            rQueue.getCache().clear();
+                            try {
+                                JSONObject jsonObject = new JSONObject(new String(response.data));
+                                Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                jsonObject.toString().replace("\\\\","");
+
+                                if (jsonObject.getString("status").equals("true")) {
+                                    Log.d("come::: >>>  ","yessssss");
+                                    arraylist = new ArrayList<HashMap<String, String>>();
+                                    JSONArray dataArray = jsonObject.getJSONArray("data");
+
+
+                                    for (int i = 0; i < dataArray.length(); i++) {
+                                        JSONObject dataobj = dataArray.getJSONObject(i);
+                                    }
+
+
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+
+                /*
+                 * If you want to add more parameters with the image
+                 * you can do it here
+                 * here we have only one parameter with the image
+                 * which is tags
+                 * */
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("title",titleET.getText().toString());
+                    params.put("description",descET.getText().toString());
+                    params.put("classroom",Integer.toString(class_id));
+                    int year = datePicker.getYear();
+                    int month = datePicker.getMonth();
+                    month = month + 1;
+                    int day = datePicker.getDayOfMonth();
+                    params.put("deadline",year+"-"+month+"-"+day);
+                    return params;
+                }
+
+                /*
+                 *pass files using below method
+                 * */
+                @Override
+                protected Map<String, VolleyMultipartRequest.DataPart> getByteData() {
+                    Map<String, DataPart> params = new HashMap<>();
+
+                    params.put("file", new DataPart(pdfname ,inputData));
+                    return params;
+                }
+            };
+
+
+            volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    0,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            rQueue = Volley.newRequestQueue(HomeworkCreationDialog.this);
+            rQueue.add(volleyMultipartRequest);
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
+    }
+
+    class DataPart {
+        private String fileName;
+        private byte[] content;
+        private String type;
+
+        public DataPart() {
+        }
+
+        DataPart(String name, byte[] data) {
+            fileName = name;
+            content = data;
+        }
+
+        String getFileName() {
+            return fileName;
+        }
+
+        byte[] getContent() {
+            return content;
+        }
+
+        String getType() {
+            return type;
+        }
 
     }
 }
